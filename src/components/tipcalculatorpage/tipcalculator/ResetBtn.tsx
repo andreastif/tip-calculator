301bb4df70254a860ecf81f3eef102c3a925528a
andreastif <45339645+andreastif@users.noreply.github.com>
2024-06-01 16:55:56 +0200

    cleanup

--- src/components/tipcalculatorpage/tipcalculator/ResetBtn.tsx
+++ src/components/tipcalculatorpage/tipcalculator/ResetBtn.tsx
@@ -1,7 +1,7 @@
 export const ResetBtn = ({ onReset, children }: { onReset: () => void; children?: React.ReactNode }) => {
   return (
     <div className="flex flex-col mx-auto px-4 py-2 gap-2">
-      <button className="btn bg-slate-300/90" onClick={() => onReset()}>
+      <button className="btn bg-slate-300/90" onClick={onReset}>
         {children}
       </button>
     </div>
--- src/components/tipcalculatorpage/tipcalculator/Summary.tsx
+++ src/components/tipcalculatorpage/tipcalculator/Summary.tsx
@@ -1,12 +1,11 @@
 import { Bill } from "./Types";
 
-export const Summary = ({ bill, children }: { bill: Bill; children?: React.ReactNode }) => {
+export const Summary = ({ bill }: { bill: Bill }) => {
   const tips = bill.price * (bill.tips[0].value / 100 + bill.tips[1].value / 100);
   const total = bill.price + tips;
 
   return (
     <div className="text-center my-3">
-      {children}
       You pay <span className="font-semibold">{total.toFixed(2)}$</span> ({bill.price.toFixed(2)}$ + {tips.toFixed(2)}$
       tip)
     </div>
